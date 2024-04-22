519cce0565969b422558c68a39304331165be7a6
monsterooo <createthink.net@gmail.com>
2024-04-22 23:17:49 +0800

    增加基础解析和写入

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,12 @@
+use anyhow;
 use clap::Parser;
-use std::path::Path;
-#[derive(Debug, Parser)]
-#[command(name="rcli", version, author, about, long_about = None)]
-struct Opts {
-    #[command(subcommand)]
-    cmd: SubCommand,
-}
+use rcli::{process_csv, Opts, SubCommand};
 
-#[derive(Debug, Parser)]
-enum SubCommand {
-    #[command(name = "csv", about = "convert csv to format.")]
-    Csv(CsvOpts)
-}
-
-#[derive(Debug, Parser)]
-struct CsvOpts {
-    #[arg(short, long, value_parser = verify_input_file)]
-    input: String,
-    #[arg(short, long, default_value = "output.json")]
-    output: String,
-    #[arg(short, long, default_value_t = ',')]
-    delimiter: char,
-    #[arg(long, default_value_t = true)]
-    header: bool,
-}
-
-fn main() {
-    let opts = Opts::parse();
-    println!("{:?}", opts)
-}
-
-fn verify_input_file(filename: &str) -> Result<String, String> {
-    if Path::new(filename).exists() {
-        Ok(filename.into())
-    } else {
-        Err("File does not exist".into())
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
     }
+
+    Ok(())
 }
