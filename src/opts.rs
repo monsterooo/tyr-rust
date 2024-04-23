a7da47359bdba60161e9cff1d5a042b5ee054b2a
monsterooo <createthink.net@gmail.com>
2024-04-23 10:20:57 +0800

    add format

--- src/opts.rs
+++ src/opts.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::path::Path;
+use std::{path::Path, str::FromStr};
 
 #[derive(Debug, Parser)]
 #[command(name="rcli", version, author, about, long_about = None)]
@@ -14,12 +14,21 @@ pub enum SubCommand {
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+    Toml
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
     #[arg(short, long, default_value = "output.json")]
     pub output: String,
+    #[arg(short, long, value_parser = parse_format, default_value = "json")]
+    pub format:  OutputFormat,
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
     #[arg(long, default_value_t = true)]
@@ -31,5 +40,33 @@ fn verify_input_file(filename: &str) -> Result<String, &'static str> {
         Ok(filename.into())
     } else {
         Err("File does not exist")
+    }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(value: OutputFormat) -> Self {
+        match value {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+            OutputFormat::Toml => "toml"
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            "toml" => Ok(OutputFormat::Toml),
+            v => anyhow::bail!("Unsupport format: {}", v)
+
+        }
     }
 }
--- src/process.rs
+++ src/process.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
-use serde_json;
+use serde_json::Value;
 use std::fs;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -21,9 +21,11 @@ pub struct Player {
 pub fn process_csv(input: &str, output: &str) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::new();
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
     let json = serde_json::to_string_pretty(&ret)?;
     fs::write(output, json)?;
